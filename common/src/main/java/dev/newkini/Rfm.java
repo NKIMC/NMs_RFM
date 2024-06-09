4ab67e977e1fb6339cd48d2de95cfef6e932084d
miyaiho <mimiccube663@gmail.com>
2024-06-09 16:37:47 +0900

    Add: json datas

--- common/src/main/java/dev/newkini/Rfm.java
+++ common/src/main/java/dev/newkini/Rfm.java
@@ -1,9 +1,17 @@
 package dev.newkini;
 
+import dev.newkini.common.registry.BlockRegistry;
+import dev.newkini.common.registry.ItemRegistry;
+import dev.newkini.common.registry.TabRegistry;
+
 public final class Rfm {
     public static final String MOD_ID = "rfm";
 
     public static void init() {
-        // Write common init code here.
+        /** Registry register */
+        ItemRegistry.ITEMS.register();
+        TabRegistry.TABS.register();
+        BlockRegistry.register();
+
     }
 }
