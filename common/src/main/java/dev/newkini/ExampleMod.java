11419ecbb642a31e1f4376afc0f110b585b456e9
miyaiho <mimiccube663@gmail.com>
2024-06-09 14:56:21 +0900

    Init: All

---
+++ common/src/main/java/dev/newkini/ExampleMod.java
@@ -0,0 +1,9 @@
+package dev.newkini;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "rfm";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
