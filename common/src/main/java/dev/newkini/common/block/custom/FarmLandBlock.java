6d5abd5962697af94beb88c92107461f4fd3e87b
newkincode <maainnewkin59@gmail.com>
2024-06-11 20:52:37 +0900

    Add: 기타 파일 추가

--- common/src/main/java/dev/newkini/common/block/custom/FarmLandBlock.java
+++ common/src/main/java/dev/newkini/common/block/custom/FarmLandBlock.java
@@ -2,8 +2,14 @@ package dev.newkini.common.block.custom;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.tags.FluidTags;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.FarmBlock;
+import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class FarmLandBlock extends FarmBlock {
     public FarmLandBlock(Properties properties) {
@@ -17,5 +23,16 @@ public class FarmLandBlock extends FarmBlock {
             }
         }
         return false;
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState p_51222_, BlockGetter p_51223_, BlockPos p_51224_, CollisionContext p_51225_) {
+        VoxelShape voxel = Block.box(0.0,0.0,0.0,16.0,13.0,16.0);
+        return voxel;
+    }
+
+    @Override
+    public RenderShape getRenderShape(BlockState p_49232_) {
+        return RenderShape.MODEL;
     }
 }
--- common/src/main/java/dev/newkini/common/registry/FoodRegistry.java
+++ common/src/main/java/dev/newkini/common/registry/FoodRegistry.java
@@ -3,5 +3,5 @@ package dev.newkini.common.registry;
 import net.minecraft.world.food.FoodProperties;
 
 public class FoodRegistry {
-    public static final FoodProperties TOMATO = (new FoodProperties.Builder()).nutrition(1).saturationModifier(0.2F).build();
+    public static final FoodProperties TOMATO = (new FoodProperties.Builder()).nutrition(4).saturationModifier(0.2F).build();
 }
